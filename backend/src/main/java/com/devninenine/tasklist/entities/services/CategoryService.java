5665372bf76e58751a9d484e64d3469b0a8a6980
Clovis Picoli <clovispicoli@gmail.com>
2021-02-04 16:39:44 -0300

    Database seeding

--- backend/src/main/java/com/devninenine/tasklist/entities/services/CategoryService.java
+++ backend/src/main/java/com/devninenine/tasklist/entities/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devninenine.tasklist.entities.Category;
 import com.devninenine.tasklist.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<Category> fikndAll(){
 		return repository.findAll();
 	}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_category (name) VALUES ('Personal');
+INSERT INTO tb_category (name) VALUES ('Work');
+INSERT INTO tb_category (name) VALUES ('Home');
