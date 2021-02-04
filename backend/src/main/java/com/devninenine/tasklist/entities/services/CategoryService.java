41eaa1d22b26c13c6cf1c91d0da0d060ecfca03f
Clovis Picoli <clovispicoli@gmail.com>
2021-02-04 17:34:59 -0300

    Delete category

--- backend/src/main/java/com/devninenine/tasklist/entities/services/CategoryService.java
+++ backend/src/main/java/com/devninenine/tasklist/entities/services/CategoryService.java
@@ -7,11 +7,14 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devninenine.tasklist.dto.CategoryDTO;
 import com.devninenine.tasklist.entities.Category;
+import com.devninenine.tasklist.entities.services.exceptions.DatabaseException;
 import com.devninenine.tasklist.entities.services.exceptions.ResourceNotFoundException;
 import com.devninenine.tasklist.repositories.CategoryRepository;
 
@@ -52,6 +55,18 @@ public class CategoryService {
 		}
 		catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }
