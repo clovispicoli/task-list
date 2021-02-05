7d518f61d1e7fcb1e7efca64f7fa331aaac0bd3e
Clovis Picoli <clovispicoli@gmail.com>
2021-02-05 03:46:11 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Navbar from './core/components/Navbar';
+import './core/assets/styles/custom.scss';
+import './app.scss';
 
 const App = () => {
-    return <h1>App</h1>
+    return (
+        <Navbar />
+    );
 }
 
 export default App;
